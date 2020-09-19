e6cb86ea68a2a6eec89f55adaec943823b34b341
Lamar Daughma <lite5h4dow@gmail.com>
2020-09-19 12:02:26 +0100

    From Links to actual files because im stupid

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,1 +1,10 @@
-/home/lotus/.config/polybar/launch.sh
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar main &
